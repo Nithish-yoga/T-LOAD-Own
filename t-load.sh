e10d99fd6c7b308590a023b9ff9ead6f691049bc
Mani @ Nithish <106460709+Nithish-yoga@users.noreply.github.com>
2022-06-22 19:50:44 +0530

    Update t-load.sh

--- t-load.sh
+++ t-load.sh
@@ -27,9 +27,9 @@ echo -e "\e[1m\e[31m\
               ██║      ███████╗╚██████╔╝██║  ██║██████╔╝
               ╚═╝      ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ v 1.1"
 echo " "  
-echo -e  "                  \e[1m\e[32m▂▃▄▅▆▇▓▒░Coded By \e[31mN17R0 \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo -e  "                  \e[1m\e[32m▂▃▄▅▆▇▓▒░Coded By \e[0;95mNithish \e[1m\e[32m░▒▓▇▆▅▄▃▂"
 echo "                      -------------------------"
-echo -e  "            \e[1m\e[32m--------->[\e[31mYOUTUBE- Noob Hackers\e[1m\e[32m]<----------"  
+echo -e  "            \e[1m\e[32m--------->[\e[31mNithish KUmar\e[1m\e[32m]<----------"  
 echo " "
 echo ""
 sleep 8.0
